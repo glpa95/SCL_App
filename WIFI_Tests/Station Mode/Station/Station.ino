645201558ed7c4d49207061662a1beb230162502
glpa95 <50981728+glpa95@users.noreply.github.com>
2019-07-08 21:51:09 +0200

    Add files via upload

---
+++ WIFI_Tests/Station Mode/Station/Station.ino
@@ -0,0 +1,28 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "TP-LINK_A1B21C";
+const char* password = "5991_29Pascal";
+
+IPAddress staticIP(192,168,1,22);
+IPAddress gateway(192,168,1,9);
+IPAddress subnet(255,255,255,0);
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  Serial.printf("Connecting to %s\n", ssid);
+  WiFi.begin(ssid, password);
+  WiFi.config(staticIP, gateway, subnet);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {}
---
+++ WIFI_Tests/WiFiAccessPoint/WiFiAccessPoint_2/WiFiAccessPoint_2.ino
@@ -0,0 +1,62 @@
+/* Create a WiFi access point and provide a web server on it. */
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "ESPap";
+const char *password = "123456789";
+
+ESP8266WebServer server(80);
+
+// Funktionsprototypen
+void handleRoot();             
+void handleLED();
+void handleNotFound();
+
+void setup() 
+{
+  //Pinbelegung 
+  pinMode(D3, OUTPUT);
+  Serial.begin(115200);
+  delay(500);
+  Serial.println('\n');
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  Serial.print("IP address of the webserver: ");
+  Serial.println(WiFi.localIP());
+ 
+    if (MDNS.begin("esp8266"))         // Start the mDNS responder for esp8266.local
+    {              
+        Serial.println("mDNS responder started");
+    } 
+    else 
+    {
+        Serial.println("Error setting up MDNS responder!");
+    }
+  server.on("/", HTTP_GET, handleRoot);     // Call the 'handleRoot' function when a client requests URI "/"
+  server.on("/LED", HTTP_POST, handleLED);  // Call the 'handleLED' function when a POST request is made to URI "/LED"
+  server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+
+  server.begin();                           // Actually start the server
+  Serial.println("HTTP server started");
+}
+void loop() 
+{ server.handleClient();}
+
+void handleRoot() 
+{server.send(200, "text/html", "<form action=\"/LED\" method=\"POST\"><input type=\"submit\" value=\"Toggle LED\"></form>");}
+
+void handleLED()                            // If a POST request is made to URI /LED
+{ digitalWrite(D3,!digitalRead(D3));        // Change the state of the LED
+  server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
+  server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect}
+  
+void handleNotFound()
+{ server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request}
